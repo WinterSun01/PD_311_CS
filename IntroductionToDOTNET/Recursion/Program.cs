f2ccead98e5c27f675ba00b0782240b050cdffb0
Анастасия Романова <nastya-danilyuk9@mail.ru>
2024-06-24 18:22:50 +0500

    Power DONE

--- IntroductionToDOTNET/Recursion/Program.cs
+++ IntroductionToDOTNET/Recursion/Program.cs
@@ -20,17 +20,25 @@ namespace Recursion
 			//int floor = Convert.ToInt32(Console.ReadLine());
 			//Elevator(floor);
 
-			Console.Write("Введите число: ");
+			//Console.Write("Введите число: ");
+			//int n = Convert.ToInt32(Console.ReadLine());
+			//try
+			//{
+			//	Console.WriteLine($"\n-----------------------\n{n}! = {Factorial(n)}");
+			//}
+			//catch (Exception ex)
+			//{
+			//	Console.WriteLine($"{ex.GetType()}: {ex.Message}");
+			//}
+
+			Console.Write("Введите основание степени: ");
+			int a = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Введите показатель степени: ");
 			int n = Convert.ToInt32(Console.ReadLine());
-			try
-			{
-				Console.WriteLine($"\n-----------------------\n{n}! = {Factorial(n)}");
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine($"{ex.GetType()}: {ex.Message}");
-			}
-		}
+            Console.WriteLine(Power(a, n));
+			Main(args);
+
+        }
 		static void Function()  //Callee - вызываемая функция
 		{
 			Console.WriteLine("Привет!");
@@ -53,6 +61,13 @@ namespace Recursion
 			long f = n * Factorial(n - 1);
 			Console.WriteLine($"{n}! = {f}");
 			return f;
+		}
+		static double Power(double a, int n)
+		{
+			return n == 0 ? 1 : n > 0 ? a * Power(a, n - 1) : 1/Power(a, -n);
+			//if (n == 0) return 1;
+			//else if (n > 0) return a * Power(a, n - 1);
+			//else return 1 / Power(a, -n);
 		}
 	}
 }
